fb791cefe0ba3177a10995e343a204aa2eff8079
nathan <nathan.consolo17@gmail.com>
2024-08-14 11:39:10 -0300

    Comecando projeto

---
+++ aula4/src/App.tsx
@@ -0,0 +1,11 @@
+// export default para que um arquivo ou função possa ser importado por outros arquivos
+export default function App()
+{
+  return (
+
+    <div>
+      <h1>Olá Mundo!</h1>
+    </div>
+
+  );
+}
