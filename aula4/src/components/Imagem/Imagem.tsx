38e60639bd28ca263876d25446b5a0c224a353fe
Julio-CRodrigues <juliocesarccr29@gmail.com>
2024-08-14 12:45:16 -0300

    Import de componentes, imgs. Manipulando componentes.

---
+++ aula4/src/components/Imagem/Imagem.tsx
@@ -0,0 +1,10 @@
+import userImg from "../../img/user.png";
+
+
+export default function Imagem() {
+  return (
+   <>
+    <img src={userImg} alt="Usuários"/>
+   </>
+  );
+}
---
+++ aula4/src/components/Principal/Principal.tsx
@@ -0,0 +1,16 @@
+import Imagem from "../Imagem/Imagem";
+import Lista from "../Lista/Lista";
+
+export default function Principal(){
+    return (
+        <main>
+
+            <h2>Conteúdo</h2>
+            <div>
+                <Imagem/>
+            </div>
+            <Lista/>
+        </main>
+    );
+
+}
